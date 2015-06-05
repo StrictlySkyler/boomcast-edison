d33a70decc695ce308f692785b0d244ba9d154ef
root <root@ubilinux.(none)>
2015-06-05 22:21:49 +0000

    Added pressure sensors!
    Four of them for now.  Just a demo, so hardcoded.

--- boomcast.js
+++ boomcast.js
@@ -3,14 +3,40 @@ var PythonShell = require('python-shell');
 
 var orientation = new Firebase('https://boomcast.firebaseio.com/orientation');
 
-var options = {
+var gyro_options = {
   mode: 'text',
   scriptPath: '/home/boomcast/RTIMULib/Linux/python/tests'
 };
 
-var shell = new PythonShell('Fusion.py', options);
+var pressure_sensor_options = {
+  mode: 'text',
+  scriptPath: '/home/boomcast/SparkFunEdisonADC/python'
+};
 
-shell.on('message', function (message) {
+var gyro = new PythonShell('Fusion.py', gyro_options);
+
+var pressure1 = new PythonShell('sensor_1.py', pressure_sensor_options);
+var pressure2 = new PythonShell('sensor_2.py', pressure_sensor_options);
+var pressure3 = new PythonShell('sensor_3.py', pressure_sensor_options);
+var pressure4 = new PythonShell('sensor_4.py', pressure_sensor_options);
+
+gyro.on('message', function (message) {
   // console.log(message);
+  orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
+});
+
+pressure1.on('message', function (message) {
+  orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
+});
+
+pressure2.on('message', function (message) {
+  orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
+});
+
+pressure3.on('message', function (message) {
+  orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
+});
+
+pressure4.on('message', function (message) {
   orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
 });
