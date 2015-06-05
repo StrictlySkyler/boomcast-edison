bd3ad47c54cd2630259ae5d0726c90043f5ccd34
Zach Heineman <zach@heineman.info>
2015-06-04 17:48:58 -0700

    Try this method for python-shell

--- boomcast.js
+++ boomcast.js
@@ -1,9 +1,23 @@
+// var PythonShell = require('python-shell');
+
+// var path = '/home/boomcast/RTIMULib/Linux/python/tests/Fusion.py'
+
+// var shell = new PythonShell(path, { mode: 'text '});
+
+// shell.on('message', function (message) {
+//   console.log(message);
+// });
+
 var PythonShell = require('python-shell');
-
-var path = '/home/boomcast/RTIMULib/Linux/python/tests/Fusion.py'
-
-var shell = new PythonShell(path, { mode: 'text '});
-
-shell.on('message', function (message) {
-  console.log(message);
+ 
+var options = {
+  mode: 'text',
+  pythonPath: '/usr/lib/python2.7',
+  scriptPath: '/home/boomcast/RTIMULib/Linux/python/tests'
+};
+ 
+PythonShell.run('Fusion.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution 
+  console.log('results: %j', results);
 });
