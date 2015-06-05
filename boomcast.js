be6088bb5a719ad069b76b1e936f1092f8b223b0
Zach Heineman <zach@heineman.info>
2015-06-04 17:40:44 -0700

    Need to import python-shell

--- boomcast.js
+++ boomcast.js
@@ -1,8 +1,9 @@
+var PythonShell = require('python-shell');
+
 var path = '/home/boomcast/RTIMULib/Linux/python/tests/Fusion.py'
 
 var shell = new PythonShell(path, { mode: 'text '});
 
 shell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement) 
   console.log(message);
 });
