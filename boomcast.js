baa88be9aff243557aadf2a01411e07a89d70ec8
root <root@ubilinux.(none)>
2015-06-09 00:16:27 +0000

    Added control for LEDs.
    Supports 3 values
    "demo" - an simple color cycle on all LEDs
    "off" - turn off all LEDs
    "on" - stop all animations

--- boomcast.js
+++ boomcast.js
@@ -1,7 +1,37 @@
 var Firebase = require('firebase');
 var PythonShell = require('python-shell');
+var SerialPort = require("serialport").SerialPort;
+var port = "/dev/ttyMFD1";  
+var serialPort = new SerialPort(port, {  
+  baudrate: 38400  
+});
 
 var orientation = new Firebase('https://boomcast.firebaseio.com/orientation');
+var LED = new Firebase('https://boomcast.firebaseio.com/LED');
+
+serialPort.on("open", function() {
+  console.log("Serial port open");
+  LED.on('value', function (data) {
+    console.log("received data : ", data.val());
+    switch(data.val()) {
+      case 'on':
+	console.log("sending z3");
+        serialPort.write("z3");
+        break;
+      case 'off':
+        console.log("sending z1");
+        serialPort.write("z1");
+        break;
+      case 'demo':
+        console.log("sending z2")
+        serialPort.write("z2");
+        break;
+      default:
+        console.log("sending "+data.val());
+        serialPort.write(data.val());
+    }
+  });
+});
 
 var gyro_options = {
   mode: 'text',
@@ -40,3 +70,5 @@ pressure3.on('message', function (message) {
 pressure4.on('message', function (message) {
   orientation.push({ 'timestamp': Date.now(), 'time': Date(), 'orientation': message });
 });
+
+
